e587e800f8fb3785447bc985db97918e82d95316
Andrew Robinson <andrewjrobinson@gmail.com>
2013-06-14 22:23:17 +1000

    Nightly: improved Python interface to TPGSettings - Improved the PyTPGSettings interface and added it to Cam module.  Still requires further work - Updated documentation and code in PyTPGBase and ExampleTPG class to use new PyTPGSettings object

--- src/Mod/Cam2/App/PyTPG/ExampleTPG/__init__.py
+++ src/Mod/Cam2/App/PyTPG/ExampleTPG/__init__.py
@@ -3,5 +3,7 @@
 # You need to import the classes that will be used as TPG's here so python can
 # find them when it loads this package
 
-from ExampleTPG import ExampleTPG, SecondTPG
+#from ExampleTPG import ExampleTPG, SecondTPG
 
+
+__all__ = ['ExampleTPG']
