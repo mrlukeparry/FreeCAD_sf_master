b74826edaa6543f86d3d35e861d54e1760faf1d9
DavidNicholls <David@Nicholls.homeip.net>
2013-07-26 16:10:08 +1000

    and more

--- src/Mod/Cam2/App/GCode.cpp
+++ src/Mod/Cam2/App/GCode.cpp
@@ -75,25 +75,18 @@ struct point_double_grammar : boost::spirit::qi::grammar<Iterator, double()>
 };
 
 
-// double mysub(boost::spirit::_1_type a, boost::spirit::_2_type b)
-double mysub(char a, double b)
-{
-	qDebug("%c %lf\n", a, b);
-	return(b);
-}
 
 namespace qi = boost::spirit::qi;
 namespace phx = boost::phoenix;
 
 typedef qi::symbols<char, double> arguments_dictionary;
-
 qi::rule<std::string::const_iterator, qi::space_type> GetRapid()
 {
 	return(qi::rule<std::string::const_iterator, qi::space_type>(qi::lexeme [+qi::char_("gG") >> *qi::char_("0") >> qi::char_("0")] >> (qi::lexeme [+qi::char_("xXyYzZ")] >> qi::double_)));
 }
 
 
-template <typename Iter, typename Skipper = qi::space_type> 
+template <typename Iter, typename Skipper = qi::blank_type> 
 	struct rs274 : qi::grammar<Iter, Skipper> 
 {
 
@@ -131,38 +124,46 @@ template <typename Iter, typename Skipper = qi::space_type>
 		LineNumberRule = (qi::lexeme [qi::char_("nN") ] >> qi::int_ )
 			[ phx::ref(line_number) = qi::_2 ];
 
-		MotionArguments = (qi::lexeme [+qi::char_("xXyYzZ")] >> qi::double_)
-			[ phx::bind(&rs274<Iter, Skipper>::Add, phx::ref(*this), qi::_1, qi::_2) ] // call arguments.Add(_1, _2);
-			// [ phx::bind(dict.add, qi::_1, qi::_2) ]
+		// X 1.1 etc.
+		MotionArgument = (qi::lexeme [+qi::char_("xXyYzZ")] >> qi::double_)
+			[ phx::bind(&rs274<Iter, Skipper>::Add, phx::ref(*this), qi::_1, qi::_2) ] // call this->Add(_1, _2);
 			;
 
 		// g00 X <float> Y <float> etc.
-		Rapid = qi::lexeme [+qi::char_("gG") >> *qi::char_("0") >> qi::char_("0")] >> +(MotionArguments)
-			[ phx::bind(&rs274<Iter, Skipper>::Print, phx::ref(*this) ) ]	// call arguments.Print()
+		Rapid = (qi::lexeme [+qi::char_("gG") >> *qi::char_("0") >> qi::char_("0")] >> +(MotionArgument) >> EndOfBlock)
+			[ phx::bind(&rs274<Iter, Skipper>::Print, phx::ref(*this) ) ]	// call this->Print()
 			;
 
 		// g01 X <float> Y <float> etc.
-		Feed = qi::lexeme [+qi::char_("gG") >> *qi::char_("0") >> qi::char_("1")] >> +(MotionArguments)
-			[ phx::bind(&rs274<Iter, Skipper>::Print, phx::ref(*this) ) ]	// call arguments.Print()
-			// [ phx::bind(&Arguments_t::Print, arguments) ]
+		Feed = (qi::lexeme [+qi::char_("gG") >> *qi::char_("0") >> qi::char_("1")] >> +(MotionArgument))
+			[ phx::bind(&rs274<Iter, Skipper>::Print, phx::ref(*this) ) ]	// call this->Print()
 			;
 
-		EndOfBlock = qi::eol
-			[ phx::bind(&rs274<Iter, Skipper>::ProcessBlock, phx::ref(*this) ) ]	// call arguments.EndOfBlock()
+			EndOfBlock = (qi::no_skip[*qi::space >> qi::eol])
+			[ phx::bind(&rs274<Iter, Skipper>::ProcessBlock, phx::ref(*this) ) ]	// call this->EndOfBlock()
 			;
 
 		MotionCommand =	
-					LineNumberRule >> Feed >> EndOfBlock
-				|	LineNumberRule >> Rapid >> EndOfBlock
+					(LineNumberRule >> EndOfBlock)
+				|	(LineNumberRule >> Feed >> EndOfBlock)
+				|	(LineNumberRule >> Rapid)
 					;
 
-		Start = MotionCommand	// [ phx::bind(&Arguments_t::Print, phx::ref(arguments) ) ]
+		Start = +(MotionCommand)	// [ phx::bind(&Arguments_t::Print, phx::ref(arguments) ) ]
 					;
+
+		BOOST_SPIRIT_DEBUG_NODE(Start);
+		BOOST_SPIRIT_DEBUG_NODE(MotionArgument);
+		BOOST_SPIRIT_DEBUG_NODE(Feed);
+		BOOST_SPIRIT_DEBUG_NODE(Rapid);
+		BOOST_SPIRIT_DEBUG_NODE(MotionCommand);
+		BOOST_SPIRIT_DEBUG_NODE(LineNumberRule);
+		BOOST_SPIRIT_DEBUG_NODE(EndOfBlock);
 	}
 
 	public:
 		qi::rule<Iter, Skipper> Start;
-		qi::rule<Iter, Skipper> MotionArguments;
+		qi::rule<Iter, Skipper> MotionArgument;
 		qi::rule<Iter, Skipper> Feed;
 		qi::rule<Iter, Skipper> Rapid;		
 		qi::rule<Iter, Skipper> MotionCommand;
@@ -188,9 +189,12 @@ int CamExport wilma()
 
 	arguments_dictionary arguments;
 	rs274<std::string::const_iterator> linuxcnc(arguments);
-	const std::string test = "N220 g0 X 1.1 Y 2.2 Z3.3   \n";
-	std::string::const_iterator begin = test.begin();
-	if (qi::phrase_parse(begin, test.end(), linuxcnc, qi::space))
+
+	const std::string gcode = "N220 g0 X 1.1 Y 2.2 Z3.3   \n";
+
+	std::string::const_iterator begin = gcode.begin();
+	// if (qi::phrase_parse(begin, gcode.end(), linuxcnc, qi::space - qi::eol))
+	if (qi::phrase_parse(begin, gcode.end(), linuxcnc, qi::blank))
 	{
 		if (arguments.find("X")) qDebug("%lf\n", arguments.at("X"));
 		if (arguments.find("Y")) qDebug("%lf\n", arguments.at("Y"));
@@ -198,206 +202,8 @@ int CamExport wilma()
 	}
 	else
 	{
-		qDebug("Parsing failed at N%d %d, %s\n", linuxcnc.line_number, std::distance(test.begin(), begin), begin);
+		qDebug("Parsing failed at N%d %d, %s\n", linuxcnc.line_number, std::distance(gcode.begin(), begin), begin);
 	}
-/*
-	// from http://stackoverflow.com/questions/3066701/boost-spirit-semantic-action-parameters
-
-	std::string input("X1.1");
-   std::string::const_iterator begin = input.begin(), end = input.end();
-
-   typedef double ArgumentRuleReturn_t;
-   qi::rule<
-      std::string::const_iterator,
-      ArgumentRuleReturn_t(double),                    //output (_val) and input (_r1)
-      qi::locals<double>,             //local int (_a)
-      ascii::space_type
-   >
-      ArgumentRule =
-            (qi::char_("xXyYzZ") >> qi::double_)
-			[
-				qi::_val = mysub(qi::as<char>(qi::_1), qi::as<double>(qi::_2))
-			];
-
-   double ruleValue, x0 = 10;
-   qi::phrase_parse(begin, end, ArgumentRule(x0), ascii::space, ruleValue);
-   std::cout << "rule value: " << ruleValue << std::endl;
-	*/
-
-
-   /*
-   std::string input("1234, 6543, 42");
-   std::string::const_iterator begin = input.begin(), end = input.end();
-
-   qi::rule<
-      std::string::const_iterator,
-      int(int),                    //output (_val) and input (_r1)
-      qi::locals<int>,             //local int (_a)
-      ascii::space_type
-   >
-      intRule =
-            (qi::int_ >> "," >> qi::int_ >> "," >> qi::int_)
-			[
-				qi::_val = (qi::_1 + qi::_2) * qi::_3 + qi::_r1
-			];
-
-   int ruleValue, x0 = 10;
-   qi::phrase_parse(begin, end, intRule(x0), ascii::space, ruleValue);
-   std::cout << "rule value: " << ruleValue << std::endl;
-   */
-
-
-   /*
-	std::string input("x1.1");
-
-	point_double_grammar<std::string::iterator> point_grammar;
-	// argument_grammar<std::string::iterator> arguments;
-
-    bool result = false;
-    double d = 0.0;
-    std::string p1 = "575040.3400";
-    std::string p2 = "117380.1200";
-    std::string p3 = "-001.22916765";
-    std::string p4 = "063.39171738";
-    std::string p5 = "2.5";
-
-    std::string::iterator it;
-    std::string::iterator last;
-
-    it = p1.begin();
-    last = p1.end();
-    result = (boost::spirit::qi::parse(it, last, point_grammar, d) && it ==
-            last);
-    if(result)
-    {
-        std::cout << p1 << " == " << d << std::endl;
-    }
-    else
-    {
-        std::cout << "Parsing failed!" << std::endl;
-    }
-
-    it = p2.begin();
-    last = p2.end();
-    result = (boost::spirit::qi::parse(it, last, point_grammar, d) && it ==
-            last);
-    if(result)
-    {
-        std::cout << p2 << " == " << d << std::endl;
-    }
-    else
-    {
-        std::cout << "Parsing failed!" << std::endl;
-    }
-
-    it = p3.begin();
-    last = p3.end();
-    result = (boost::spirit::qi::parse(it, last, point_grammar, d) && it == last);
-    if(result)
-    {
-        std::cout << p3 << " == " << d << std::endl;
-    }
-    else
-    {
-        std::cout << "Parsing failed!" << std::endl;
-    }
-
-    it = p4.begin();
-    last = p4.end();
-    result = (boost::spirit::qi::parse(it, last, point_grammar, d) && it == last);
-    if(result)
-    {
-        std::cout << p4 << " == " << d << std::endl;
-    }
-    else
-    {
-        std::cout << "Parsing failed!" << std::endl;
-    }
-
-    it = p5.begin();
-    last = p5.end();
-    result = (boost::spirit::qi::parse(it, last, point_grammar, d) && it == last);
-    if(result)
-    {
-        std::cout << p5 << " == " << d << std::endl;
-    }
-    else
-    {
-        std::cout << "Parsing failed!" << std::endl;
-    }
-	*/
-
-
-	// http://climbing-the-hill.blogspot.com.au/2010/05/boost-your-spirits.html
-
-
-	// from http://4thmouse.com/index.php/2008/07/20/boost-spirit-part-ii-attaching-actions-to-your-grammar/
-
-
-	// from http://www.codeproject.com/Articles/8516/An-Introduction-to-the-Boost-Spirit-Parser-framewo
-
-	/*
-	struct Syntax :
-		public boost::spirit::grammar<Syntax>
-	{
-	public:
-		Syntax( CParser &parser );
-		virtual ~Syntax();
-		template <typename ScannerT>
-		struct definition
-		{
-		public:
-			definition( Syntax const &self )
-			{
-				integer =
-					lexeme_d[ (+digit_p) ]
-					;
-				factor =
-					integer |
-					vars |
-					'(' >> expression >> ')' |
-					( '-' >> factor ) |
-					( '+' >> factor )
-					;
-				term =
-					factor >> *( ( '*' >> factor) | ( '/' >> factor ) )
-					;
-				expression =
-					term >> *( ( '+' >> term ) | ( '-' >> term ) )
-					;
-				assignment =
-					vars
-					>> '=' >> expression
-					;
-				var_decl =
-					lexeme_d
-					[
-						( ( alpha_p >> *( alnum_p | '_' ) )
-						- vars )[vars.add]
-					]
-					;
-				declaration =
-					"int" >> var_decl >> *( ',' >> var_decl )
-					;
-				baseExpression =
-					str_p( "exit" )[*self.m_finishedProcessing] |
-					str_p( "mod" ) >> integer |
-					declaration |
-					assignment |
-					'?' >> expression
-					;
-			}
-			boost::spirit::symbols<int> vars;
-			boost::spirit::rule<ScannerT> integer, factor, term,
-				expression, assignment, var_decl, declaration,
-				baseExpression;
-			const boost::spirit::rule<ScannerT> &start() const { return baseExpression; }
-		};
-		friend struct definition;
-	private:
-		Private::FinishedProcessing *m_finishedProcessing;
-	};
-	*/
 
 	return(0);
 }
