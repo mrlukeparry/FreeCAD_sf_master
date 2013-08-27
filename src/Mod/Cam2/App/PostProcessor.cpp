369ef756d644af6f48ccf16d68ff6977f1d02ab3
DavidNicholls <David@Nicholls.homeip.net>
2013-08-27 16:32:24 +1000

    Fixed a couple of typos.

--- src/Mod/Cam2/App/PostProcessor.cpp
+++ src/Mod/Cam2/App/PostProcessor.cpp
@@ -81,7 +81,7 @@ MachineProgram *PostProcessorInst::postProcess(ToolPath *toolpath, Item *postpro
 	// Lock the 'Global Interpreter Lock' so that we're not interrupted during our execution.
 	Base::PyGILStateLocker locker;	
 
-	// Remember where stdout and stderr was pointing before we start so that we can reinstate them later.
+	// Remember where stdout and stderr were pointing before we start so that we can reinstate them later.
 	PyObject *original_stdout = PySys_GetObject("stdout");
 	PyObject *original_stderr = PySys_GetObject("stderr");
 
@@ -106,7 +106,7 @@ MachineProgram *PostProcessorInst::postProcess(ToolPath *toolpath, Item *postpro
 		for (i=0; i<lines->size(); i++)
 		{
 			// Seed the PythonStdout object with the line offset from the toolpath QStringList
-			// so that we can tie the generated GCode with the lines in the toolpath (Python) scrip.
+			// so that we can tie the generated GCode with the lines in the toolpath (Python) script.
 			out->ToolPathIndex( QStringList::size_type(i) );
 			err->ToolPathIndex( QStringList::size_type(i) );
 			
@@ -130,7 +130,7 @@ MachineProgram *PostProcessorInst::postProcess(ToolPath *toolpath, Item *postpro
 /**
 	This method gets called when a line of Python code attempts to write to stdout.
 	We capture the string they were going to write and re-direct it to the
-	machine_program->error_string QStringList.
+	machine_program->machineProgram QStringList.
  */
 Py::Object PythonStdout::write(const Py::Tuple& args)
 {
@@ -215,7 +215,7 @@ Py::Object PythonStderr::write(const Py::Tuple& args)
                 const char* string = PyString_AsString(unicode);
 				if (buffer.endsWith(QString::fromAscii("\n")))
 				{
-					this->machine_program->addMachineCommand(buffer, this->toolpath_index);
+					this->machine_program->addErrorString(buffer);
 					buffer.clear();
 				}
                 Py_DECREF(unicode);
@@ -227,7 +227,7 @@ Py::Object PythonStderr::write(const Py::Tuple& args)
 			buffer += QString::fromStdString(string);
             if (buffer.endsWith(QString::fromAscii("\n")))
 			{
-				this->machine_program->addMachineCommand(buffer, this->toolpath_index);
+				this->machine_program->addErrorString(buffer);
 				buffer.clear();
 			}
         }
